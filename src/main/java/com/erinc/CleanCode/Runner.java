b615e3de3d6af3bf70418122435905349c9dfe81
ErincK <erinckocaoglu@gmail.com>
2023-01-11 13:20:59 +0300

    Initial commit

---
+++ src/main/java/com/erinc/CleanCode/Runner.java
@@ -0,0 +1,46 @@
+package com.erinc.CleanCode;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * Clean Code?
+         *
+         * 1- Code'un anlaşılır ve açık bir şekilde yazılması. (Code Smell)
+         * 2- Kodlarınızı (Class'ları) soyutlarken oluşturduğunuz sınıfların tek bir görev yapmasını sağlayın.
+         *      Tek Sorumluluk İlkesi (Single Responsibility).
+         * 3- Sınıflarınızda olabildiğince az method kullanılmasını sağlayın. İdeal => 10-15 Method.
+         * 4- Tek bir method, Tek bir iş yapmalıdır. Bir method'un yaptığı iş-code,
+         *      başka bir method gövdesinde copy-paste yapılmamış ve kullanılmamış ise muhtemelen doğru bir iş
+         *      yapıyorsunuzdur. Method'larda tekrar eden kodunuz asla OLMASIN!!...
+         * 5- Methodlarınızı olabildiğince küçük tutun. 15-20 satır aralığında olması tercihimzdir.
+         *      İçinde kullanacağınız değişkenleri max 2-3 değişken olacak şekilde kurgulayın.
+         * 6- Method'larda karar yapıları kullanılmaktadır. Bu karar yapılarını 7-8 aralığından az tutmak
+         *      kaydıyla kurgulamak doğrudur.
+         * 7- Method yazarken ilk başlarda küçük olabilir. Ancak ilerleyen zaman içinde gelen istekler neticesinde
+         *      method genişlemeye başlar ve okunurluğu azalır. Böyle durumlarla karşılaşmamak için kodlarınızı,
+         *      5. maddede olduğu şekilde muhafaza etmek için bölmek mantıklıdır.
+         * 8- Methodlarınız birbirlerine yakın işlemler yapabilirler. Yakın işlemler yapan kod örneklerini başka bir
+         *      methodda toplayıp diğer methodlarınızın bu yeni methodu çağırmasını sağlarsınız.
+         * 9- Proje içerisinde method isimlerinin bir standardı olmalı. Method isimlerini proje için nasıl
+         *      yazmaya karar verdiyseniz tüm proje boyunca öyle yazınız.
+         * 10- Method ismi tanımlarken, olabildiğince ingilizce kullanın. İsimlendirme yaparken,
+         *      yaptıkları işi niteleyen bir kelime kullanın ve olabildiğince emir kipi kullanarak tanımlayın.
+         * 11- Method isimlendirmesi yaparken asla kısaltma kullanmayın. UZUUUUUUUN UZUUUUUUUN YAZIN..
+         *      örn; satisSonrasiMusteriIdsiUzerindenAlinanUcretKayitlarininToplamTutari(Long musteriId);
+         * 12- Mutlaka methodlarınızın üzerine yorum satırı ekleyin. Uygulamayı kendiniz kullanacak ya da
+         *      tek başınıza yazacak bile olasanız açıklama yazmadan method yazmayınız.
+         * 13- Methodlarınıza isim verirken zorlanıyorsanız, muhtemelen kodu yanlış yazmışsınızdır ve
+         *      method birden çok iş yapıyordur.
+         * 14- Bir proje içinde projeye başlamadan, ClassName, MethodName ve DataTypeName tasarlanmalı ve
+         *      bu şekilde proje sonuna kadar devam edilmeli.
+         *
+         *
+         *
+         */
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/erinc/DesignPatern/solid/Runner.java
@@ -0,0 +1,45 @@
+package com.erinc.DesignPatern.solid;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * SOLID - ACID
+         * Amaç:
+         *
+         * 1- Yazılımın esnek olmasını ve yeniden kullanılabilir olmasını sağlamak.
+         * 2- Kodların daha anlaşılır olmasını sağlar ve okunabilirliği arttırır.
+         * 3- Kodların modülerliğini arttırarak sadece methıd bazında değil, sınıf ve
+         *      proje bazında da kodların tekrar etmesini sağlar.
+         * 4- Kodlarınızı yazarken SOLID prensiplerine uygun şekilde yazmaya özen gösterirseniz,
+         *      zaman ve maliyetten tasarruf edersiniz.
+         *      Kodları daha sonra revize etmek zorunda kalmaz ve geliştirmeye açık olursunuz.
+         *
+         *
+         *
+         *  S - Single Responsibility
+         *
+         *
+         *
+         *  O - open-closed
+         *
+         *
+         *
+         *  L - Listow Substituation
+         *
+         *
+         *
+         *  I - Interface Segregation
+         *
+         *
+         *
+         *  D - Dependency Inversion
+         *
+         *
+         */
+
+
+
+
+
+    }
+}
